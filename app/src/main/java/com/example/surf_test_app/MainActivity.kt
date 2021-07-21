bb83421279b338d5a6aca257e8757d0e00e0c198
krasjbee <zloyjb@gmail.com>
2021-07-21 11:34:34 +0700

    feat : ui draft added

--- app/src/main/java/com/example/surf_test_app/MainActivity.kt
+++ app/src/main/java/com/example/surf_test_app/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.surf_test_app
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.surf_test_app.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.srContainer.setOnRefreshListener {
+
+        }
+
     }
 }
