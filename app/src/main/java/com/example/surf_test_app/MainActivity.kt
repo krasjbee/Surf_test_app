adc276bad50bd3f202bfed4ac3ea9337d68bdd6d
krasjbee <zloyjb@gmail.com>
2021-07-26 21:49:32 +0700

    feat : test rv

--- app/src/main/java/com/example/surf_test_app/MainActivity.kt
+++ app/src/main/java/com/example/surf_test_app/MainActivity.kt
@@ -1,19 +1,36 @@
 package com.example.surf_test_app
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.surf_test_app.databinding.ActivityMainBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.srContainer.setOnRefreshListener {
 
+
+        val layoutManager = LinearLayoutManager(this, RecyclerView.VERTICAL, false)
+        val adapter = FilmsAdapter { title ->
+            Toast.makeText(this, title, Toast.LENGTH_SHORT).show()
+        }
+        binding.rvFilmList.apply {
+            this.adapter = adapter
+            this.layoutManager = layoutManager
+        }
+        val scope = CoroutineScope(Dispatchers.IO)
+        scope.launch {
+            val films = Retrofit.filmsService.getMovies().filmResults
+            launch(Dispatchers.Main) { adapter.submitList(films) }
         }
 
     }
